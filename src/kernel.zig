764b43348ca9882a0a7dec23701ae20a3347750c
fng97 <fng97@icloud.com>
2025-05-27 19:18:54 +0100

    [05] Hello World!: Use an ECALL to print hello

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,9 +1,18 @@
+/// RISC-V toy kernel based on the book 'OS in 1,000 Lines'
+
+// Some inline assembly is used. The docs are here:
+// https://ziglang.org/documentation/master/#toc-Assembly
+
 const bss = @extern([*]u8, .{ .name = "__bss" });
 const bss_end = @extern([*]u8, .{ .name = "__bss_end" });
 const stack_top = @extern([*]u8, .{ .name = "__stack_top" });
 
 export fn kernel_main() noreturn {
+    // Ensure the bss section is cleared to zero.
     @memset(bss[0 .. bss_end - bss], 0);
+
+    const hello = "Hello Kernel!\n";
+    for (hello) |c| _ = sbi_call(c, 0, 0, 0, 0, 0, 0, 1);
 
     while (true) asm volatile ("");
 }
@@ -15,4 +24,39 @@ export fn boot() linksection(".text.boot") callconv(.Naked) void {
         :
         : [stack_top] "r" (stack_top),
     );
+}
+
+const SbiRet = struct {
+    err: usize,
+    value: usize,
+};
+
+pub fn sbi_call(
+    arg0: usize,
+    arg1: usize,
+    arg2: usize,
+    arg3: usize,
+    arg4: usize,
+    arg5: usize,
+    fid: usize,
+    eid: usize,
+) SbiRet {
+    var err: usize = undefined;
+    var value: usize = undefined;
+
+    asm volatile ("ecall"
+        : [err] "={a0}" (err),
+          [value] "={a1}" (value),
+        : [arg0] "{a0}" (arg0),
+          [arg1] "{a1}" (arg1),
+          [arg2] "{a2}" (arg2),
+          [arg3] "{a3}" (arg3),
+          [arg4] "{a4}" (arg4),
+          [arg5] "{a5}" (arg5),
+          [arg6] "{a6}" (fid),
+          [arg7] "{a7}" (eid),
+        : "memory"
+    );
+
+    return .{ .err = err, .value = value };
 }
