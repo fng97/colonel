1d18703593d4623812bdea773ab12bfac4dca0cd
fng97 <fng97@icloud.com>
2025-05-31 15:27:13 +0100

    [08] Exception: Add the exception handler
    We test this by calling "unimp" to trigger an illegal instruction
    exception which invokes the exception handler.

--- src/kernel.zig
+++ src/kernel.zig
@@ -13,12 +13,13 @@ export fn kernel_main() noreturn {
     // Ensure the bss section is cleared to zero.
     @memset(bss[0 .. bss_end - bss], 0);
 
+    write_csr("stvec", @intFromPtr(&kernel_entry));
+
     console.print("\n\nHello {s}\n", .{"World!"}) catch {};
     console.print("1 + 2 = {d}, {x}\n", .{ 1 + 2, 0x1234abcd }) catch {};
 
-    @panic("Booted! Panic!\n");
-
-    // while (true) asm volatile ("");
+    asm volatile ("unimp");
+    while (true) asm volatile ("");
 }
 
 pub fn panic(
@@ -29,7 +30,7 @@ pub fn panic(
     _ = error_return_trace;
     _ = ret_addr;
 
-    console.print("PANIC: {s}\n", .{msg}) catch {};
+    console.print("PANIC: {s}", .{msg}) catch {};
     while (true) asm volatile ("");
 }
 
@@ -77,6 +78,163 @@ pub fn sbi_call(
     );
 
     return .{ .err = err, .value = value };
+}
+
+const TrapFrame = struct {
+    ra: usize,
+    gp: usize,
+    tp: usize,
+    t0: usize,
+    t1: usize,
+    t2: usize,
+    t3: usize,
+    t4: usize,
+    t5: usize,
+    t6: usize,
+    a0: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+    a6: usize,
+    a7: usize,
+    s0: usize,
+    s1: usize,
+    s2: usize,
+    s3: usize,
+    s4: usize,
+    s5: usize,
+    s6: usize,
+    s7: usize,
+    s8: usize,
+    s9: usize,
+    s10: usize,
+    s11: usize,
+    sp: usize,
+};
+
+// Preserves the CPU state by saving all general-purpose registers.
+//
+// RISC-V defines 32 integer registers. The first integer register is a zero register, and the
+// remainder are general-purpose registers. The kernel allocates extra stack space to store the
+// general purpose registers in the format described by the TrapFrame structure. `handle_trap` is
+// passed a pointer to this structure and handles the exception logic The CPU state is then restored
+// and execution is resumed.
+export fn kernel_entry() align(4) callconv(.Naked) void {
+    asm volatile (
+    // Store the original stack pointer in sscratch so that the kernel can allocate its own
+    // private stack (requiring sp).
+        \\csrw sscratch, sp
+
+        // Decrease the stack pointer to allocate space for the 31 general purpose registers (4
+        // bytes each). The resulting address in sp will be passed to handle_trap and cast to
+        // TrapFrame.
+        \\addi sp, sp, -4 * 31
+
+        // Store the remaining 30 general purpose registers (we'll store the original stack pointer
+        // further down). After this block the state is captured and the kernel can begin using
+        // these registers.
+        \\sw ra,  4 * 0(sp)
+        \\sw gp,  4 * 1(sp)
+        \\sw tp,  4 * 2(sp)
+        \\sw t0,  4 * 3(sp)
+        \\sw t1,  4 * 4(sp)
+        \\sw t2,  4 * 5(sp)
+        \\sw t3,  4 * 6(sp)
+        \\sw t4,  4 * 7(sp)
+        \\sw t5,  4 * 8(sp)
+        \\sw t6,  4 * 9(sp)
+        \\sw a0,  4 * 10(sp)
+        \\sw a1,  4 * 11(sp)
+        \\sw a2,  4 * 12(sp)
+        \\sw a3,  4 * 13(sp)
+        \\sw a4,  4 * 14(sp)
+        \\sw a5,  4 * 15(sp)
+        \\sw a6,  4 * 16(sp)
+        \\sw a7,  4 * 17(sp)
+        \\sw s0,  4 * 18(sp)
+        \\sw s1,  4 * 19(sp)
+        \\sw s2,  4 * 20(sp)
+        \\sw s3,  4 * 21(sp)
+        \\sw s4,  4 * 22(sp)
+        \\sw s5,  4 * 23(sp)
+        \\sw s6,  4 * 24(sp)
+        \\sw s7,  4 * 25(sp)
+        \\sw s8,  4 * 26(sp)
+        \\sw s9,  4 * 27(sp)
+        \\sw s10, 4 * 28(sp)
+        \\sw s11, 4 * 29(sp)
+
+        // Store the original sp at the end of the space we allocated (you can see it's the last
+        // item in TrapFrame).
+        \\csrr a0, sscratch
+        \\sw a0, 4 * 30(sp)
+
+        // Call handle_trap with a pointer to the start of TrapFrame.
+        \\mv a0, sp
+        \\call handle_trap
+
+        // Restore the general purpose registers and resume execution.
+        \\lw ra,  4 * 0(sp)
+        \\lw gp,  4 * 1(sp)
+        \\lw tp,  4 * 2(sp)
+        \\lw t0,  4 * 3(sp)
+        \\lw t1,  4 * 4(sp)
+        \\lw t2,  4 * 5(sp)
+        \\lw t3,  4 * 6(sp)
+        \\lw t4,  4 * 7(sp)
+        \\lw t5,  4 * 8(sp)
+        \\lw t6,  4 * 9(sp)
+        \\lw a0,  4 * 10(sp)
+        \\lw a1,  4 * 11(sp)
+        \\lw a2,  4 * 12(sp)
+        \\lw a3,  4 * 13(sp)
+        \\lw a4,  4 * 14(sp)
+        \\lw a5,  4 * 15(sp)
+        \\lw a6,  4 * 16(sp)
+        \\lw a7,  4 * 17(sp)
+        \\lw s0,  4 * 18(sp)
+        \\lw s1,  4 * 19(sp)
+        \\lw s2,  4 * 20(sp)
+        \\lw s3,  4 * 21(sp)
+        \\lw s4,  4 * 22(sp)
+        \\lw s5,  4 * 23(sp)
+        \\lw s6,  4 * 24(sp)
+        \\lw s7,  4 * 25(sp)
+        \\lw s8,  4 * 26(sp)
+        \\lw s9,  4 * 27(sp)
+        \\lw s10, 4 * 28(sp)
+        \\lw s11, 4 * 29(sp)
+        \\lw sp,  4 * 30(sp)
+        \\sret
+    );
+}
+
+export fn handle_trap(_: *TrapFrame) void {
+    const scause = read_csr("scause");
+    const stval = read_csr("stval");
+    const user_pc = read_csr("sepc");
+
+    std.debug.panic(
+        "Unexpected trap scause={x}, stval={x}, user_pc={x}\n",
+        .{ scause, stval, user_pc },
+    );
+}
+
+/// Read control and status register (CSR)
+fn read_csr(comptime register: []const u8) usize {
+    return asm volatile ("csrr %[ret], " ++ register
+        : [ret] "=r" (-> usize),
+    );
+}
+
+/// Write control and status register (CSR)
+fn write_csr(comptime register: []const u8, val: usize) void {
+    asm volatile ("csrw " ++ register ++ ", %[val]"
+        :
+        : [val] "r" (val),
+    );
 }
 
 const console: std.io.AnyWriter = .{
