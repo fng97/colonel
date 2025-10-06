85f232fc9c928fef843ebe82e8d17b2d6cca0d52
fng97 <fng97@icloud.com>
2025-10-06 17:25:06 +0100

    [11] Page Table: Separate cmds for QEMU and LLDB
    We want access to both the QEMU and LLDB consoles.

--- build.zig
+++ build.zig
@@ -26,8 +26,7 @@ pub fn build(b: *std.Build) void {
     exe_check.setLinkerScript(b.path("src/kernel.ld"));
     check.dependOn(&exe_check.step);
 
-    const run_step = b.step("run", "Run with QEMU");
-    const run_cmd = b.addSystemCommand(&.{
+    const qemu_argv = .{
         "qemu-system-riscv32",
         "-machine",
         "virt",
@@ -37,38 +36,28 @@ pub fn build(b: *std.Build) void {
         "mon:stdio",
         "--no-reboot",
         "-nographic",
-        "-kernel",
-    });
+    };
+
+    const run_step = b.step("run", "Run in QEMU");
+    const run_cmd = b.addSystemCommand(&qemu_argv);
+    run_cmd.addArg("-kernel");
     run_cmd.addArtifactArg(exe);
     // Kept doing 'zig build run' and then working with the elf in zig-out without realising it
     // wasn't being updated. Let's make sure it's installed every time.
-    run_cmd.step.dependOn(b.getInstallStep());
+    run_step.dependOn(b.getInstallStep());
     run_step.dependOn(&run_cmd.step);
 
-    const debug_step = b.step("debug", "Debug with QEMU and LLDB");
-    // FIXME: Using bash to manage the two processes for now. I'm sure there's a better way. Maybe
-    // we should create two separate steps, one for starting QEMU in debug mode and another for
-    // connecting LLDB. That way, we could work from both in separate shells.
-    const debug_cmd = b.addSystemCommand(&.{
-        "bash", "-c",
-        \\set -euxo pipefail
-        \\
-        // Same command as above (except we specify "kernel.elf" path) until the next comment.
-        \\qemu-system-riscv32 \
-        \\      -machine virt \
-        \\      -bios default \
-        \\      -serial mon:stdio \
-        \\      --no-reboot \
-        \\      -nographic \
-        \\      -kernel zig-out/bin/kernel.elf \
-        // Listen for debug connection.
-        \\      -gdb tcp::1234 \
-        // Stall CPU at startup so that it is started by the debugger.
-        \\      -S \
-        // Background QEMU and start LLDB.
-        \\      & lldb -o "target create zig-out/bin/kernel.elf" \
-        \\           -o "gdb-remote localhost:1234"
-    });
-    debug_cmd.step.dependOn(b.getInstallStep());
+    const debug_step = b.step("debug", "Run in QEMU, open GDB port, and await connection");
+    const debug_cmd = b.addSystemCommand(&qemu_argv);
+    debug_cmd.addArgs(&.{ "-gdb", "tcp::1234", "-S" }); // debug args
+    debug_cmd.addArg("-kernel");
+    debug_cmd.addArtifactArg(exe);
+    debug_step.dependOn(b.getInstallStep());
     debug_step.dependOn(&debug_cmd.step);
+
+    const lldb_step = b.step("lldb", "Run LLDB, attach to QEMU");
+    const lldb_cmd = b.addSystemCommand(&.{ "lldb", "-o" });
+    lldb_cmd.addPrefixedArtifactArg("target create ", exe);
+    lldb_cmd.addArgs(&.{ "-o", "gdb-remote localhost:1234" });
+    lldb_step.dependOn(&lldb_cmd.step);
 }
